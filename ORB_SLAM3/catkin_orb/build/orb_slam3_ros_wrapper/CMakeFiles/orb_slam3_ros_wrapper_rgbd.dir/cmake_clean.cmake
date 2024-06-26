40beadc1bdff86e4a067bf16feb07dd1f132fbab
Narathip Rodwarna <nrodwarna@gmail.com>
2024-06-26 02:37:32 -0400

    re-organize

---
+++ ORB_SLAM3/catkin_orb/build/orb_slam3_ros_wrapper/CMakeFiles/orb_slam3_ros_wrapper_rgbd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/ORB_SLAM3/catkin_orb/devel/.private/orb_slam3_ros_wrapper/lib/orb_slam3_ros_wrapper/orb_slam3_ros_wrapper_rgbd"
+  "/ORB_SLAM3/catkin_orb/devel/.private/orb_slam3_ros_wrapper/lib/orb_slam3_ros_wrapper/orb_slam3_ros_wrapper_rgbd.pdb"
+  "CMakeFiles/orb_slam3_ros_wrapper_rgbd.dir/src/common.cc.o"
+  "CMakeFiles/orb_slam3_ros_wrapper_rgbd.dir/src/rgbd_node.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orb_slam3_ros_wrapper_rgbd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ORB_SLAM3/catkin_orb/devel/setup.sh
@@ -0,0 +1,1 @@
+/ORB_SLAM3/catkin_orb/devel/.private/catkin_tools_prebuild/setup.sh
